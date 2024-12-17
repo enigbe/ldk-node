b4c84c679b908bfd70c5a1198871ca38e7ff9eef
Enigbe <ochekliyeenigbe@gmail.com>
2024-12-17 05:19:13 +0100

    refactor: simplify Debug trait implementations for LogWriterConfig
    - Add a custom Debug implementation for LogWriterConfig to work around NodeBuilder's
      Debug constraints.
    - Remove the Debug trait from LogWriter, as it is no longer needed due to the custom Debug
       impl on LogWriterConfig.
    - Remove the Debug implementation from Writer for consistency.

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -262,7 +262,6 @@ pub(crate) enum TestLogWriter {
 }
 
 /// Simple in-memory mock `log` logger for tests.
-#[derive(Debug)]
 pub(crate) struct MockLogger {
 	logs: Arc<Mutex<Vec<String>>>,
 }
@@ -299,10 +298,6 @@ impl Log for MockLogger {
 	fn flush(&self) {}
 }
 
-/// [`MockLogger`] as custom logger - a destination for [`Writer::CustomWriter`]
-/// to write logs to.
-///
-/// [`Writer::CustomWriter`]: ldk_node::logger::Writer::CustomWriter
 impl LogWriter for MockLogger {
 	fn log(&self, record: LogRecord) {
 		let message = format!(
